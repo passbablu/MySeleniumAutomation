925d7f5d2777cdac88fdd1fd7b35411e417ea4d0
ferdos bablu <ferdos736@gmail.com>
2019-08-05 23:53:29 -0400

    humayon

---
+++ MySeleniumAutomation/src/main/java/GitPushing.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class GitPushing {
+
+//	public static void main(String[] args) {
+//		for (int i=0; i<7;i++) {
+//			
+//			System.out.println("Eid on Sunday "+i );
+//		}
+@Test
+	public void gitpushandpull () {
+	for (int i=0; i<7;i++) {
+		
+		System.out.println("Eid on Sunday "+i );
+	
+	
+} 
+	
+}
+	}
+
+
--- MySeleniumAutomation/src/main/java/MyFirstCommit.java
+++ MySeleniumAutomation/src/main/java/MyFirstCommit.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 		System.out.println("eid is on sunday " +i);
 	}
 			
-		
+	System.out.println("today is monday");	
 	
 }
 }
